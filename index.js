fe8bcd5f527c0a1a6121066470e337314de32943
TastyTuna87 <86468081+TastyTuna87@users.noreply.github.com>
2022-04-08 12:55:50 +0200

    1
    1

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-let myLibrary = [
-  {
-    title: "Cim",
-    author : "Szerzo",
-    pages : "250"
-  }
-];
+let myLibrary = [];
 
 let bookTitle = document.getElementById("bookTitle");
 let bookAuthor = document.getElementById("bookAuthor");
@@ -12,7 +6,10 @@ let bookPages = document.getElementById("bookPages");
 
 const addButton = document.getElementById("addBook");
 const container = document.getElementById("list");
-const lineContainer = document.createElement("div");
+const isRead = document.getElementById("read");
+const isNotRead = document.getElementById("unread");
+const bookNumber =document.getElementById("booksNumber");
+
 
 function Book(title, author, pages) {
   // the constructor...
@@ -20,63 +17,70 @@ function Book(title, author, pages) {
   this.author = author
   this.pages = pages
 }
-
 function addBookToLibrary() {
   addButton.addEventListener("click", ()=>{
       const book = new Book(bookTitle.value, bookAuthor.value, bookPages.value);
       myLibrary.push(book);
-    library();
+      addBooks();
+      console.log(myLibrary)
   })
 }
-function library(){
-  lineContainerDiv();
-  addBookName();
-  addBookAuthor();
-  addBookPages();
-  radioButton();
-  removeButton();
-}
-function lineContainerDiv(){
-    lineContainer.className="lineContainer";
-    container.appendChild(lineContainer);
-}
-function addBookName(){
-  let bookName = document.createElement("p");
-  bookName.className = "bookName";
-  for(let i =0; i <myLibrary.length; i++){
-    bookName.innerHTML = JSON.stringify(myLibrary[i].title)
-  }
-  lineContainer.appendChild(bookName)
-}
-function addBookAuthor(){
-  let bookAuthor = document.createElement("p");
-  bookAuthor.className = "bookAuthor";
-  for(let i =0; i <myLibrary.length; i++){
-    bookAuthor.innerHTML = JSON.stringify(myLibrary[i].author)
-  }
-  lineContainer.appendChild(bookAuthor)
-}
-function addBookPages(){
-  let bookPages = document.createElement("p");
-    bookPages.className = "bookPages";
-    for(let i =0; i <myLibrary.length; i++){
-      bookPages.innerHTML = JSON.stringify(myLibrary[i].pages)
-    }
-    lineContainer.appendChild(bookPages);
-}
-function radioButton(){
+function addBooks(){
   const radButton = document.createElement("input");
-  radButton.className = "radioButton";
-  radButton.setAttribute("type", "checkbox");
-  lineContainer.appendChild(radButton);
-}
-function removeButton(){
   const remButton = document.createElement("button")
-  remButton.className = "removeButton";
-  remButton.innerHTML ="Remove";
-  lineContainer.appendChild(remButton);
-  remButton.addEventListener("click", () =>{
-    remButton.parentElement.remove();
-  })
+  let bookName = document.createElement("p");
+  let bookAuthor = document.createElement("p");
+  let bookPages = document.createElement("p");
+  const bookDiv = document.createElement("div");
+
+  let userReadBooks =0;
+  let userUnreadBooks = 0;
+  isRead.innerHTML = userReadBooks ;
+  let userBooks = myLibrary.length;
+  bookNumber.innerHTML = userBooks;
+
+    for(let i =0; i <myLibrary.length; i++){
+      bookName.innerHTML = myLibrary[i].title
+      bookAuthor.innerHTML = myLibrary[i].author
+      bookPages.innerHTML = myLibrary[i].pages
+    }
+      radButton.className = "radioButton";
+      radButton.setAttribute("type", "checkbox");
+      radButton.id = "radButton";
+      
+      remButton.className = "removeButton";
+      remButton.innerHTML ="Remove";
+      bookDiv.append(bookName, bookAuthor, bookPages, radButton, remButton);
+
+    bookDiv.className = "bookDiv";
+    container.appendChild(bookDiv);
+
+    remButton.addEventListener("click", () =>{
+      remButton.parentElement.remove();
+      isNotRead.innerHTML = userUnreadBooks -1;
+      bookNumber.innerHTML = userBooks -1;
+      
+      for(let i =0; i< myLibrary.length; i++){
+        const index = myLibrary.findIndex(object => {
+          return object.title === bookTitle.value;
+        });
+          myLibrary.splice(index,1);
+          return myLibrary, console.log(myLibrary, index)
+      }
+      
+    })
+  for(let i = 0; i < myLibrary.length; i++){
+    userUnreadBooks+=1;
+    isNotRead.innerHTML = userUnreadBooks;
+  }      
+    radButton.addEventListener("click", ()=>{
+      if(radButton.checked){
+        
+      }else{
+        
+      }
+    })
 }
+
 addBookToLibrary();
+
